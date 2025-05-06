28e923dd96ca31e29386e31c6d91bde9960b892d
Geovana <geovanamscardoso183@gmail.com>
2025-05-06 09:54:26 -0300

    Criando JS e Quiz

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,27 @@
 
 let imagens=[
     'src/assets/imagem1.jpg', 
-    'src/assets/imagem2',
-    'src/assets/imagem3',
+    'src/assets/imagem2.jpg',
+    'src/assets/imagem3.jpg',
 ];
+
+//Declarando as variáveis
+
+let i=0;
+
+//Tempo em js é contato em milissegundos
+let tempo =3000; //3 segundos
+
+//Criando a função slideshow
+function slideshow(){
+    document.getElementById('image').src=imagens[i];
+    i++;
+
+    if(i == imagens.length){
+        i=0;
+    }
+
+    setTimeout("slideshow()", tempo);
+}
+slideshow();
 
